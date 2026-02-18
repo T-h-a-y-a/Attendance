7c301ce824c7a0034bff3ebdc56e74f860721c0e
T-h-a-y-a <ictstaff07@gmail.com>
2026-02-18 10:41:32 +0530

    Enhance Vite config with build settings
    Added build configuration and adjusted chunk size warning limit.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
-  }
+    port: 5173,
+  },
+  build: {
+    // default warning is 500 kB
+    chunkSizeWarningLimit: 100000, // increase to 1.5 MB (adjust as you want)
+  },
 });
